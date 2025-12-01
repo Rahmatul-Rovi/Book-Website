be31319c34bb21b0ed564e44ef21f3a349537986
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2025-12-01 10:24:31 +0600

    create banner

---
+++ src/Pages/Books/Books.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+
+const Books = () => {
+    const [allBooks, setAllBooks] = useState([]);
+
+    useEffect(() => {
+     fetch("bookData.json")
+     .then(res => res.json())
+     .then(data => {
+        setAllBooks(data);
+     })
+    }, []);
+    return (
+        <div>
+            <h1>Hello I have books</h1>
+        </div>
+    );
+};
+
+export default Books;
